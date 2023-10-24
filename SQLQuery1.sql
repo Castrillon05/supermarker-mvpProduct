da91664f6f54cf4c74f4d3dfe36910d5c95e6478
danny castrillon muñoz <dannycastrillon2@gmail.com>
2023-10-24 00:49:15 -0500

    Initial Project

---
+++ SQLQuery1.sql
@@ -0,0 +1,10 @@
+﻿use Supermarket_mvpProvider
+create table Provider
+(
+ Provider_Id int identity (100000,1) primary key,
+ Provider_Name nvarchar (50) not null,
+ Provider_Observation nvarchar (50) not null,
+)
+go
+insert into Provider values('Cash', 'Cash mode')
+insert into Provider values('Credit card', 'Credit card mode')
---
+++ practica 1/practica 1/Program.cs
@@ -0,0 +1,73 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        // tener en cuenta sapa
+        Console.WriteLine("me llamo danny"); //Este es un comentario de una sola línea.
+        Console.WriteLine(3 + 3 / 8); /*Este es un comentario de varias líneas*/
+        Console.Write("Hola mundo");
+        Console.WriteLine(" como estas ");
+        String Nombre = "Danny";
+        Console.WriteLine(Nombre);
+        int Numero = 23;
+        Console.WriteLine(Numero);
+        int Numero_2;
+        Numero_2 = 25;
+        Console.WriteLine(Numero_2);
+        int Numero_3 = 12;
+        Numero_3 = 30;//El numero es 30
+        Console.WriteLine(Numero_3);
+        int Numero_4 = 10;
+        Console.WriteLine(Numero_4);
+        double Decimal = 33.33D;
+        Console.WriteLine(Decimal);
+        char Letra = 'D';
+        Console.WriteLine(Letra);
+        bool respuesta_c = false;
+        Console.WriteLine(respuesta_c);
+        String Apellido = "Castrillon";
+        Console.WriteLine(Apellido);
+        const int Numero_5 = 60;
+        /*Numero_5 = 90;*/
+        String Apodo = "Panini";
+        Console.WriteLine("hola  " + Apodo);
+        String Primer_Nom = "Robeto";
+        String Segundo_Nom = "Hugo";
+        String Nombre_Compl = Primer_Nom  +  Segundo_Nom;
+        Console.WriteLine(Nombre_Compl);
+        int x = 15;
+        int y = 15;
+        Console.WriteLine(x + y);
+        int z = 50, f = 20, g = 100;
+        Console.WriteLine(z - f + g );
+        int a, b, c;
+        a = b = c = 20;
+        Console.WriteLine(a + b + c);
+        int  Valor = 10000;
+        Console.WriteLine(Valor);
+        long Valor_2 = 1827346982376291866L;
+        Console.WriteLine( Valor_2 );
+        float valor = 23.4523F;
+        Console.WriteLine( valor );
+        double valor_2 = 234.2345D;
+        Console.WriteLine( valor_2 );
+        float f1 = 35e5F;  //la e indica cuantos ceros quieres que se te aumenten a ala derecha
+        double d1 = 45E4D; //la E indica cuantos ceros quieres que se te aumenten a ala derecha 
+        Console.WriteLine(f1);
+        Console.WriteLine(d1);
+        bool Es_divertido_cshar = true;
+        bool El_pescado_es_rico = false;
+        Console.WriteLine(Es_divertido_cshar);
+        Console.WriteLine(El_pescado_es_rico);
+        char mi_sexo_es = 'M';
+        Console.WriteLine(mi_sexo_es);
+        String Nom_res = "Leños y sabores";
+        Console.WriteLine(Nom_res);
+        int Numero_6 = 43;
+        double pasar_num = Numero_6;
+        Console.WriteLine(Numero_6);
+        Console.WriteLine(pasar_num);
+    }
+
+}
